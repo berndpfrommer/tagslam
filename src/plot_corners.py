0232f00d35606fb30b0b8b813278939398bb8edb
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2019-06-13 08:07:14 -0400

    added feature to adjust for image center

--- src/plot_corners.py
+++ src/plot_corners.py
@@ -17,6 +17,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='plot tag corners')
     parser.add_argument('-t','--times', nargs='+',
                         help='<Required> ros times to plots', required=True)
+    parser.add_argument('--cx', '-x', action='store', type = float,
+                        default=0, help='principal point u coordinate.')
+    parser.add_argument('--cy', '-y', action='store', type = float,
+                        default=0, help='principal point v coordinate.')
     parser.add_argument('--file', '-f', action='store',
                         default=expanduser("~")+'/.ros/tag_corners.txt',
                         help='file with corners.')
@@ -37,7 +41,7 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots()
     for i in range(va.shape[0]):
-        ax.scatter(va[i,4], -va[i,5], color=col[va[i,0]])
+        ax.scatter(va[i,4]-args.cx, args.cx-va[i,5], color=col[va[i,0]])
         ax.annotate("%d:%d:%d" % (int(va[i,1]), int(va[i,2]),
                                   int(va[i,3])), (va[i,4], -va[i,5]))
     ax.set_aspect('equal', adjustable='box')
