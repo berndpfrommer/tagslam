49eb8f6ddc8e7611c77f62961e49641f92bfc160
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2019-08-01 10:05:54 -0400

    improved profiler formatting

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -39,8 +39,10 @@ namespace tagslam {
       float stddev = sqrt((double) sqdn);
       int64_t dmin = (pt.count > 0) ? pt.min.count() : -1;
       int64_t dmax = (pt.count > 0) ? pt.max.count() : -1;
-      os << std::setw(maxlen + 1) << std::left << it->first << "= tot: " << std::setw(9) << pt.duration.count() << "us per: "
-         << dn << "+-" << (int)stddev << "(" << dmin << "-" << dmax << ")" << " count: " << pt.count << " " << std::endl;
+      os << std::setw(maxlen + 1) << std::left << it->first << "= tot: "
+         << std::setw(9) << std::right << pt.duration.count() << "us per: "
+         << dn << "+-" << (int)stddev << "(" << dmin << "-" << dmax << ")"
+         << " count: " << pt.count << " " << std::endl;
     }
     return os;
   }
